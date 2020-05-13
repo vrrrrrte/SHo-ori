a1f9c22058c1c8389273886632017ef809e1359e
crusheddd <mak@MacBook-Pro-Mak.local>
2020-05-13 21:25:24 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,54 @@ import logo from './logo.svg';
 import pic from './pic.jpg';
 import './App.css';
 
-function App() {
+
+const notifications = [
+  {
+    text: "Added the new video in Core Workout section",
+    img: "https://i.imgur.com/S43qYm8.png",
+    type: "video"
+  },
+  {
+    text: "Boulder shoulders workout in Combos for you",
+    type: "combo"
+
+  },
+  {
+    text: "Chest Pump workout in Combos for you",
+    type: "combo"
+  },
+  {
+    text: "Added the new video in Forearm Workout section",
+    type: "video"
+  }
+]
+
+const Notification = ({ notification: { type, text, img } }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={pic} className="App-logo" alt="logo" />
-      </header>
+    <div className="notification">
+      <img src={img} className="picture" />
+      {type === 'video' ?
+        <div className="text">
+          Added the new video in
+          <div className="bold">Core Workout</div>
+          section
+        </div>
+        :
+        <div style={{ color: 'white' }}>combo</div>
+      }
     </div>
-  );
+  )
+
 }
 
-export default App;
+
+const NotificationsList = () => (
+  <div className="app">
+    {notifications.map((notification) =>
+      <Notification notification={notification} />
+    )}
+  </div>
+)
+
+
+export default NotificationsList;
---
+++ src/Picture.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import pic from './pic.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pic} className="App-logo" alt="logo" />
+	<input />
+      </header>
+    </div>
+  );
+}
+
+export default App;
